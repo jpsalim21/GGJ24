3143ed52547d43c8991c7a79a431a5ad31e2516a
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-01-27 00:18:14 -0300

    Implementa galinhaController

---
+++ GGJ 2024/Assets/Scripts/GalinhaController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GalinhaController : MonoBehaviour
+{
+    [System.Serializable] struct StructEstado
+    {
+        public EstadosGalinha estado;
+        public MonoBehaviour scriptReferente;
+    }
+    [SerializeField] StructEstado[] listaEstados;
+    StructEstado estadoAtual;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeState(EstadosGalinha novoEstado)
+    {
+        if(novoEstado == estadoAtual.estado)
+        {
+            return;
+        }
+        estadoAtual.scriptReferente.enabled = false;
+        switch (novoEstado)
+        {
+            case EstadosGalinha.normal:
+                break;
+            case EstadosGalinha.fuzil:
+                break;
+            case EstadosGalinha.bota:
+                break;
+            default:
+                break;
+        }
+
+    }
+}
