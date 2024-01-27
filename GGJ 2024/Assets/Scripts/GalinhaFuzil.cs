add872ef4dc54df5f80ee67e120c56130151de0a
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-01-26 23:24:08 -0300

    commit inicial

---
+++ GGJ 2024/Assets/Scripts/GalinhaFuzil.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalinhaFuzil : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed, timeDash;
+    float timeDashPassed = 0;
+    bool dashing = false;
+    Vector2 direction = Vector2.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector2.up;
+            timeDashPassed = timeDash;
+            dashing = true;
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2.left;
+            timeDashPassed = timeDash;
+            dashing = true;
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+            timeDashPassed = timeDash;
+            dashing = true;
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector2.down;
+            timeDashPassed = timeDash;
+            dashing = true;
+        }
+        if(timeDashPassed > 0)
+        {
+            timeDashPassed -= Time.deltaTime;
+        } else if (dashing)
+        {
+            dashing = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (dashing)
+        {
+            rb.velocity = direction * speed;
+        } else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+}
---
+++ GGJ 2024/Assets/Scripts/GalinhaPadrao.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalinhaPadrao : MonoBehaviour
+{
+    Vector2 input;
+    Rigidbody2D rb;
+    [SerializeField] float speed;
+
+    bool dash = false;
+    [SerializeField] float dashTime, dashSpeed;
+    float dashTimePassed = 0;
+    Vector2 dashDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input.Normalize();
+        if (Input.GetKeyDown(KeyCode.Space) && dashTimePassed <= 0)
+        {
+            dashTimePassed = dashTime;
+            dash = true;
+            dashDirection = input;
+        }
+        if(dashTimePassed > 0)
+        {
+            dashTimePassed -= Time.deltaTime;
+        } else if (dash)
+        {
+            dash = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (!dash)
+        {
+            rb.velocity = input * speed;
+        } else
+        {
+            rb.velocity = dashDirection * dashSpeed;
+        }
+    }
+}
