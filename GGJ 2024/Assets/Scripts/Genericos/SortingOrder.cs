639bdf1f6477a543671146cc48fe2c5a005ade23
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-01-27 12:44:50 -0300

    Sorting Order + tileset

---
+++ GGJ 2024/Assets/Scripts/Genericos/SortingOrder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrder : MonoBehaviour
+{
+    [SerializeField] bool estatico = false;
+    SpriteRenderer sp;
+
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        if (estatico)
+        {
+            sp.sortingOrder = Mathf.RoundToInt(transform.position.y * -100);
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sp.sortingOrder = Mathf.RoundToInt(transform.position.y * -100);
+    }
+}
