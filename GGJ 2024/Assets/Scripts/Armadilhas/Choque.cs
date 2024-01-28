3daf384dd48a0eb1718f03319b3799b2dbe5588b
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-01-28 14:52:13 -0300

    AAAAAAAAAAAAAAAAA

---
+++ GGJ 2024/Assets/Scripts/Armadilhas/Choque.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choque : MonoBehaviour
+{
+    [SerializeField] GameObject setActive;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 9 && !GalinhaController.gc.voando)
+        {
+            setActive.transform.position = GalinhaController.gc.gameObject.transform.position;
+            setActive.SetActive(true);
+            GalinhaController.gc.Reviver();
+            StartCoroutine("volta");
+        }
+    }
+
+    IEnumerator volta()
+    {
+        yield return new WaitForSeconds(0.7f);
+        setActive.SetActive(false);
+    }
+
+}
